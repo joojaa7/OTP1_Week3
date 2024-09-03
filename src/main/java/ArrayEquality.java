5561b4cc9e26bc56423d395f164cc8ce9f752eb1
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-09-03 11:07:30 +0300

    m

--- src/main/java/ArrayEquality.java
+++ src/main/java/ArrayEquality.java
@@ -1,4 +1,42 @@
+import java.util.Arrays;
+
 public class ArrayEquality {
+
+    public static boolean areArraysEqual(int[] a1, int[] a2) {
+        if (a1 == null || a2 == null) {
+            return a1 == a2;
+        }
+        if (a1.length != a2.length) {
+            return false;
+        }
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areMultidimensionalArraysEqual(int[][] a1, int[][] a2) {
+        if (a1 == null || a2 == null) {
+            return a1 == a2;
+        }
+        if (a1.length != a2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < a1.length; i++) {
+            for (int j = 0; j < a1[i].length; j++) {
+                if (a1[i][j] != a2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
 
     public int[] getSingleDimensionArray() {
         return new int[]{2, 3, 5, 7};
@@ -6,5 +44,11 @@ public class ArrayEquality {
 
     public int[][] getMultiDimensionArray() {
         return new int[][]{{2, 3}, {5, 7}, {11, 13}};
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 2, 3 }, { 5, 7 }, { 11, 13 } };
+        int[][] matrix1 = { { 2, 3 }, { 5, 7 }, { 11, 13 } };
+        System.out.println(areMultidimensionalArraysEqual(matrix, matrix1));
     }
 }
